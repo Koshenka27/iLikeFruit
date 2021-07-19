71ff79caa14aba0907409e85a5128d70f345471f
Logan Koshenka <koshenka27@gmail.com>
2021-07-19 14:27:53 -0400

    Initial Commit

---
+++ ILikeFruit/ILikeFruitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ILikeFruitApp.swift
+//  ILikeFruit
+//
+//  Created by Logan Koshenka on 7/19/21.
+//
+
+import SwiftUI
+
+@main
+struct ILikeFruitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
